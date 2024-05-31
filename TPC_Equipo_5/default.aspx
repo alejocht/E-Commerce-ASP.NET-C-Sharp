863d538e8009ce673b99fc24f139ccf4248ad4c4
Santiago Escobar <santiago.escobar97@gmail.com>
2024-05-31 20:39:42 -0300

    Bootstrap y site-Master
    - Se agregaron librerias CSS y JS de Bootstrap, de forma local.
    - Se creo la clase master con su primer pagina default.
    
    Queda pendiente mejorar la master...

---
+++ TPC_Equipo_5/default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/site.Master" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="TPC_Equipo_5._default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container" id="containerPrincipal">
+        <h1>TEXTO DE PRUEBA</h1>
+    </div>
+</asp:Content>
---
+++ TPC_Equipo_5/site.Master.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPC_Equipo_5
+{
+    public partial class site : System.Web.UI.MasterPage
+    {
+        string busqueda;
+
+        protected bool ValidarTextBox(string busqueda)
+        {
+            if (string.IsNullOrEmpty(busqueda)) { return false; }
+            else { return true; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["listaArticulosEnCarrito"] == null)
+            {
+                Contador.Text = "0";
+            }
+            else
+            {
+                //Pasar contador de carrito a la vista
+            }
+        }
+
+        protected void btnBusqueda_Click(object sender, EventArgs e)
+        {
+            busqueda = txtBusqueda.Text;
+            if (ValidarTextBox(busqueda))
+            {
+                //caso en el que tiene que iniciar la busqueda
+                Response.Redirect("default.aspx?busqueda=" + busqueda, false);
+            }
+            else
+            {
+                //caso en el que tiene que mostrar todo
+                Response.Redirect("default.aspx", false);
+            }
+        }
+    }
+}
