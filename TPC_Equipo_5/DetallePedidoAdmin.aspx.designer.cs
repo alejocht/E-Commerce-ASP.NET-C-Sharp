d920e75cac4d2e1614e50ded0460e34d5201a8e6
Santiago Escobar <santiago.escobar97@gmail.com>
2024-07-04 17:42:16 -0300

    update detallePedido Buttons
    - Se agrega modals de confirmacion a los botones ya creados.
    - Se agrega un diseño de modo oscuro en los modals (se agrega tambien en la confirmacion de cerrar sesión de paso)

--- TPC_Equipo_5/DetallePedidoAdmin.aspx.designer.cs
+++ TPC_Equipo_5/DetallePedidoAdmin.aspx.designer.cs
@@ -87,12 +87,12 @@ namespace TPC_Equipo_5
         protected global::System.Web.UI.WebControls.Button btnActualizar;
 
         /// <summary>
-        /// btnCancelar control.
+        /// Button1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnCancelar;
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
